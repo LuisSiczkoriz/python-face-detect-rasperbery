75c82002a639e4461a22c8cd6a6baa9f1d946268
William Tenório <william.tenorio@loopix.com.br>
2019-07-08 20:27:10 -0300

    ajustes

--- zipFiles.py
+++ zipFiles.py
@@ -5,7 +5,17 @@ pathRoot = os.getcwd()
 source = os.path.join( pathRoot, 'files' )
 destination = os.path.join( pathRoot, 'upload/files.zip' )
 
+def generateFolderDefault():
+    folderName = 'upload'
+
+    if not os.path.exists( os.path.join( pathRoot, folderName ) ):
+        os.makedirs( os.path.join( pathRoot, folderName ) )
+    
+    return os.path.normpath( os.path.join( pathRoot, folderName ) )
+
 def zipAll( source, destination ):
+    generateFolderDefault()
+    
     base = os.path.basename( destination )
     name = base.split('.')[0]
     format = base.split('.')[1]
