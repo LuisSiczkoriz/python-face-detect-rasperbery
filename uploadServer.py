685fe6e8ce6836f3cb7b728de5f503f87d1c00aa
William Tenório <william.tenorio@loopix.com.br>
2019-07-09 22:58:50 -0300

    ajustes uploadServer

--- uploadServer.py
+++ uploadServer.py
@@ -1,14 +1,33 @@
-#$pip install paramiko os
 import paramiko
 import os
 import sys
 
 pathRoot = os.getcwd()
+source = os.path.join( pathRoot, 'upload' )
+source = os.path.join( source, 'files.zip' )
+dest = '/home/ubuntu/'
 
-HOSTNAME = 'ssh.ec2-52-55-113-15.compute-1.amazonaws.com'
-USERNAME = 'ubuntu'
-KEY_FILE = os.path.join( pathRoot, 'key-semparar-server-dsa.ppk' )
+keyRSA = os.path.join( pathRoot, 'keyAws.pem' )
+hostname = "ec2-52-55-113-15.compute-1.amazonaws.com"
+port = '22'
+username = "ubuntu"
 
+try:
+    keyConnect = paramiko.RSAKey.from_private_key_file( keyRSA )
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.WarningPolicy)
+    
+    client.connect( hostname = hostname, username = username, pkey = keyConnect )
 
-system(cd /var/www/html/)
-scp -i <key_path> -r * root@<my_local_machine_public_ip:~/var/www/html/
+    sftp = client.open_sftp()
+    sftp.put(source, dest)
+
+    stdin, stdout, stderr = client.exec_command('pwd')
+    print(stdout.read())
+
+finally:
+    print("connected")
+    client.close()
+    sftp.close()
+
